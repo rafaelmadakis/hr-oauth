c7e889ddced61ea50b654f34ef537955cef551ba
Rafael Madakis <rafaelmadakis@gmail.com>
2020-10-26 21:06:20 -0300

    hr-payroll implementação mock

---
+++ hr-payroll/src/main/java/com/madakis/hrpayroll/entities/Payment.java
@@ -0,0 +1,22 @@
+package com.madakis.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Payment implements Serializable {
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+
+
+    public double getTotal() {
+        return days * dailyIncome;
+    }
+
+}
