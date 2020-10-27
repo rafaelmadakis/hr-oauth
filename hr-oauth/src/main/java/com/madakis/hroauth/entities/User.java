db6f9b90a1e46420f34f8f7d498cb2f54fdc85bf
Rafael Madakis <rafaelmadakis@gmail.com>
2020-10-27 17:13:14 -0300

    Login JWT Token

--- hr-oauth/src/main/java/com/madakis/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/madakis/hroauth/entities/User.java
@@ -2,13 +2,18 @@ package com.madakis.hroauth.entities;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
-public class User implements Serializable {
+public class User implements UserDetails,Serializable {
 
     @EqualsAndHashCode.Include
     private Long id;
@@ -21,4 +26,35 @@ public class User implements Serializable {
     private String password;
 
     private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x ->
+                new SimpleGrantedAuthority(x.getRoleName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
