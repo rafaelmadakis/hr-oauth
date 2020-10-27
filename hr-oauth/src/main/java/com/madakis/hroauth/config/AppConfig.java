53f8d7c1f341792264293d7fdd74eb51960116ad
Rafael Madakis <rafaelmadakis@gmail.com>
2020-10-27 16:47:30 -0300

    JWT

---
+++ hr-oauth/src/main/java/com/madakis/hroauth/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.madakis.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Para trabalhar com segurança/JWT
+     */
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("MY-SECRETE-KEY");
+        return tokenConverter;
+    }
+
+    /**
+     * JWT/Segurança
+     */
+    @Bean
+    public JwtTokenStore TokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
+    }
+}
